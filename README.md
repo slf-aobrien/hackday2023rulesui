902360ca3ed6ae03b4a84349e7f5c6771214a560
slf-aobrien <aaron.obrien@sunlife.com>
2023-10-09 07:26:23 -0500

    WIP testing

--- README.md
+++ README.md
@@ -33,3 +33,36 @@ npm run storybook
 ```
 npm install @mui/material @emotion/react @emotion/styled
 ```
+
+# Unit Testing
+Projects created with Vite don't have testing setup by default.  I found this [website](https://www.robinwieruch.de/vitest-react-testing-library/) to set this up:
+Run the following command to bring the library into our project
+```
+npm install vitest --save-dev
+npm install jsdom --save-dev
+npm install @testing-library/react @testing-library/jest-dom --save-dev
+```
+Update the package.json file:
+Currently it looks like this:
+```
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "lint": "eslint . --ext js,jsx --report-unused-disable-directives --max-warnings 0",
+    "preview": "vite preview",
+    "storybook": "storybook dev -p 6006",
+    "build-storybook": "storybook build"
+  },
+```
+We need to configure a test script so add this line somwhere (I like after build)
+```
+    "test": "vitest",
+```
+Run with this command:
+```
+npm run test
+```
+## Notes
+Test files need to have the same extension as the file they are testing [Stack Overflow](https://stackoverflow.com/questions/74970340/vitest-with-react-testing-library-unexpected-token )
+
+[Useful walk through](https://www.robinwieruch.de/react-testing-library/) of the vite testing library
---
+++ src/components/memberCard/MemberCard.test.jsx
@@ -0,0 +1,72 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeEach, afterEach, global, jest} from 'vitest';
+
+import MemberCard from "./MemberCard";
+import { fireEvent, userEvent } from "@storybook/testing-library";
+import { act } from 'react-dom/test-utils';
+
+function validateWithServer(user){
+    console.log("Validate With Server called")
+    console.log(user)
+}
+
+describe("MemberCard", () => {
+    it("renders Member Card component", () => {
+        // render(<MemberCard validateData = {validateWithServer}/>);
+        render (<MemberCard validateData = {(user) => {
+                console.log(user)
+        }} />);
+        const firstNameField = screen.getByLabelText("First Name");
+        const lastNameField = screen.getByLabelText("Last Name");
+        const birthdayField = screen.getByLabelText("Birthday");
+        const occupationCodeField = screen.getByLabelText("Occupation Code");
+
+        expect(firstNameField).toBeInTheDocument();
+        expect(lastNameField).toBeInTheDocument();
+        expect(birthdayField).toBeInTheDocument();
+        expect(occupationCodeField).toBeInTheDocument();
+
+        fireEvent.change(firstNameField,{
+            persist: {}},
+            target: { value: 'Aaron'},
+        });
+        fireEvent.change(lastNameField,{
+            persist: jest.fn(),
+            target: { value: 'OBrien'},
+        });
+
+        //firstNameField.focus();
+        //userEvent.type(firstNameField, 'Aaron');
+        
+
+        screen.debug(firstNameField);
+        screen.debug(lastNameField);
+
+        // const aDay = 'someday';
+        // const code = 'riksy';
+        //firstNameField.value = 'Aaron';
+        //lastNameField.value = 'OBrien';
+        // fireEvent.change(firstNameField,{
+        //     target: { value: 'Aaron'},
+        // });
+  
+        // fireEvent.change(birthdayField,{
+        //     target: { value: aDay},
+        // });
+        // fireEvent.change(occupationCodeField,{
+        //     target: { value: code},
+        // });
+
+        expect (firstNameField.value).toBe("Aaron");
+        // expect (lastNameField.value).toBe("OBrien");
+       // expect (birthdayField.value).toBe("someday");
+        //expect (occupationCodeField.value).toBe("risky");
+
+        const button = screen.getByText("Validate");
+        expect(button).toBeInTheDocument();
+        fireEvent.click(button);
+    });
+  });
+
+  
+  
